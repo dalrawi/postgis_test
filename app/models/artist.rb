6682b9fe2eb0f1860e20df0799fdd777986a42e8
dalrawi <dalrawi@nmsu.edu>
2018-03-12 14:08:39 -0600

    artist assets commit

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,3 +1,8 @@
 class Artist < ApplicationRecord
 	has_and_belongs_to_many :places
+	has_and_belongs_to_many :tags
+	
+	def self.search(keyword)
+		Artist.where("name ilike ?", "%#{keyword}%")
+	end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,15 @@ class User < ApplicationRecord
                     	uniqueness: { case_sensitive: false }
 	has_secure_password
 	validates :password, presence: true, length: { minimum: 6 }	
+
+	def self.from_omniauth(auth)
+	    where(provider: auth.provider, uid: auth.uid).first_or_initialize.tap do |user|
+	      user.provider = auth.provider
+	      user.uid = auth.uid
+	      user.username = auth.info.name
+	      user.oauth_token = auth.credentials.token
+	      user.oauth_expires_at = Time.at(auth.credentials.expires_at)
+	      user.save!
+	    end
+  end
 end
