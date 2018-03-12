3b2895caede5ca71e37880ad9cc919b19625efa7
dalrawi <dalrawi@nmsu.edu>
2018-03-12 14:10:01 -0600

    populated seeds.rb with test data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+artists_list = ["Marathon Of Disappointed","Astute Rodent","Twinkie Dichotomy","Chronic Parallax","Sunlit Shroom","Platinum Spatula","Bio Dab","Assessment Puppy"]
+tags_list = ["techno","electronic","soundcloud rapper","FL studio","electrofunk","classical","indie","post apocalyptic glam metal"]
+artists_list.each do |band|
+	Artist.create(name: band)
+end
+tags_list.each do |genre|
+	Tag.create(name: genre)
+end
