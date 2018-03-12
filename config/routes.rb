25459c4e4135fafab5d01cbe106814916d584938
dalrawi <dalrawi@nmsu.edu>
2018-03-12 14:09:16 -0600

    added search routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
-  root 'users#new'
+  root 'users#index'
+
+  get 'artists/index'
+  #get 'artists_controller/index'
+
+  match 'artists/index', to: 'artists#index', via: :post
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
